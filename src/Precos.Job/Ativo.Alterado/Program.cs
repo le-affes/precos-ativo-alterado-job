7352a32134411fb0fdcb0f7509b73f06e06ae681
le-affes <leandro.arcanjo@usp.br>
2026-06-28 16:11:34 -0300

    Criação do projeto + arquivos de deploy

---
+++ src/Precos.Job/Ativo.Alterado/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ativo.Alterado;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        var app = builder.Build();
+
+        app.MapGet("/hello", () => "API .NET funcionando");
+
+        app.MapGet("/health", () => Results.Ok(new
+        {
+            status = "UP"
+        }));
+
+        app.Run();
+    }
+}
