ab438a401ff3d36a9dfebc66c3d58bedf3e540c4
le-affes <leandro.arcanjo@usp.br>
2026-07-12 19:50:56 -0300

    Teste de alteração de fila

--- src/Precos.Job/Ativo.Alterado/Service/SqsService.cs
+++ src/Precos.Job/Ativo.Alterado/Service/SqsService.cs
@@ -1,6 +1,7 @@
 ﻿using Amazon.SQS;
 using Amazon.SQS.Model;
 using Ativo.Alterado.Service.DTOs;
+using Microsoft.Extensions.Logging;
 using System.Text.Json;
 
 namespace Ativo.Alterado.Service;
@@ -15,7 +16,7 @@ public class SqsService : ISqsService
     {
         _sqsClient = sqsClient;
 
-        _operacaoRegistradaQueueUrl = Environment.GetEnvironmentVariable("SQS_ATIVO_ALTERADO_URL")
+        _operacaoRegistradaQueueUrl = Environment.GetEnvironmentVariable("SQS_OPERACAO_REGISTRADA_URL")
             ?? throw new InvalidOperationException("Variável de ambiente SQS_OPERACAO_REGISTRADA_URL não configurada.");
         _logger = logger;
     }
